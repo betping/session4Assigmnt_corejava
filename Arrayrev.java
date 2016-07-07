017474ac9f756c804dd2677c4af9365473501a4e
betping <betping1993@gmail.com>
2016-07-08 03:33:33 +0530

    Add files via upload

---
+++ Arrayrev.java
@@ -0,0 +1,45 @@
+
+public class Arrayrev {
+	      
+	  {
+	   
+		  public static void reverse(double[] array) {
+	 
+	          double [] anArray;              // declares an array of floating point numbers
+	 
+	          anArray = new double[6];      // allocates memory for 6 floating point numbers
+	 
+	              
+	
+	           anArray[0] = 5.0; // initialize first element
+	 
+	           anArray[1] = 4.4; // initialize second element
+	 
+	           anArray[2] = 1.9; // etc.
+	 
+	           anArray[3] = 2.9;
+	 
+	           anArray[4] = 3.4;
+	 
+	           anArray[5] = 3.5;
+	 
+	   
+	 
+	           //print the original array order
+	
+	           System.out.println("Element at index 0: " + anArray[0]);
+	 
+	           System.out.println("Element at index 1: " + anArray[1]);
+	 
+	           System.out.println("Element at index 2: " + anArray[2]);
+	 
+	           System.out.println("Element at index 3: " + anArray[3]);
+	 
+	           System.out.println("Element at index 4: " + anArray[4]);
+	 
+	           System.out.println("Element at index 5: " + anArray[5]);
+	 
+	      }
+
+}
+}
